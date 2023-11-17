fbf1e2f11b8211f962f47d43218c292c0e02fe25
0nullpointer <nullpointer@skiff.com>
2023-11-17 14:39:15 +0100

    Add og params

--- src/testggml/test.cpp
+++ src/testggml/test.cpp
@@ -5,8 +5,10 @@
 
 char *test_ggml_init(char *arg) {
     struct ggml_init_params params_ggml;
-    params_ggml.mem_size   = ggml_tensor_overhead();
-    params_ggml.no_alloc   = true;
+    size_t ctx_size = 0;
+    ctx_size = 176012928;
+    params_ggml.mem_size   = ctx_size;
+    params_ggml.no_alloc   = false;
     params_ggml.mem_buffer = NULL;
     ggml_init(params_ggml);
     return (char *)"after ggml init from native";
