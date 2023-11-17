e053888fdcee108111631411ccdd3c0bbec8d623
0nullpointer <nullpointer@skiff.com>
2023-11-17 17:43:07 +0100

    Add params from gguf_init_from_file

--- src/testggml/test.cpp
+++ src/testggml/test.cpp
@@ -6,9 +6,9 @@
 char *test_ggml_init(char *arg) {
     struct ggml_init_params params_ggml;
     size_t ctx_size = 0;
-    ctx_size = 176012928;
+    ctx_size = 67200;
     params_ggml.mem_size   = ctx_size;
-    params_ggml.no_alloc   = false;
+    params_ggml.no_alloc   = true;
     params_ggml.mem_buffer = NULL;
     ggml_init(params_ggml);
     return (char *)"pong";
