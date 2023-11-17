f2fbddd3c42e5e53d3ad2c3dc9d8401cf2d134ac
0nullpointer <nullpointer@skiff.com>
2023-11-17 15:03:24 +0100

    Add clip model load method

--- src/testggml/test.cpp
+++ src/testggml/test.cpp
@@ -1,15 +1,21 @@
-
-#include "ggml.h"
+#include "clip.h"
 #include "test.h"
 
 
 char *test_ggml_init(char *arg) {
-    struct ggml_init_params params_ggml;
-    size_t ctx_size = 0;
-    ctx_size = 176012928;
-    params_ggml.mem_size   = ctx_size;
-    params_ggml.no_alloc   = false;
-    params_ggml.mem_buffer = NULL;
-    ggml_init(params_ggml);
+
+    // Test GGML Init
+    // struct ggml_init_params params_ggml;
+    // size_t ctx_size = 0;
+    // ctx_size = 176012928;
+    // params_ggml.mem_size   = ctx_size;
+    // params_ggml.no_alloc   = false;
+    // params_ggml.mem_buffer = NULL;
+    // ggml_init(params_ggml);
+
+    // Test CLIP model load
+    char *filename = (char *)"model.bin";
+    int verbosity = 1;
+    clip_model_load(filename, verbosity);
     return (char *)"pong";
 }
